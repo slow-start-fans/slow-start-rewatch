3ee75a57e83ec9980eb62a45d189a231be7ad571
vaclav-2012 <38167104+vaclav-2012@users.noreply.github.com>
2020-05-29 00:54:06 +0200

    Implement "--version" CLI option to show the current version
    - Add `importlib_metadata` to dependencies
    - Add the `version` module to retrieve the version number the Python package
    - Add the "--version" CLI option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,4 @@ We follow [Semantic Versions](https://semver.org/).
 - Initial release
 - Use double quotes for string literals
 - Set up CLI with Click
+- Implement "--version" CLI option to show the current version
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,8 +6,17 @@ from slow_start_rewatch.__main__ import main
 
 
 def test_run_successfully():
-    """Test launching the program."""
+    """Test launching the program without params."""
     runner = CliRunner()
 
     cli_result = runner.invoke(main)
     assert cli_result.exit_code == 0
+
+
+def test_check_version():
+    """Test the launch with the ``--version`` option."""
+    runner = CliRunner()
+
+    cli_result = runner.invoke(main, ["--version"])
+    assert cli_result.exit_code == 0
+    assert "slow-start-rewatch, version" in cli_result.output
