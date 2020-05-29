b7bdeb050d9e827e152a2a6db38ca1bea6ed49dc
vaclav-2012 <38167104+vaclav-2012@users.noreply.github.com>
2020-05-30 01:13:12 +0200

    Add class `SlowStartRewatchException`

---
+++ slow_start_rewatch/exceptions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import Optional
+
+
+class SlowStartRewatchException(Exception):
+    """An exception that slow_start_rewatch can handle."""
+
+    def __init__(self, message: str, hint: Optional[str] = None, exit_code=1):
+        """Initialize SlowStartRewatchException."""
+        super().__init__(message)
+        self.message = message
+        self.hint = hint
+        self.exit_code = exit_code
+
+    def __str__(self):
+        """Return string representation of the exception."""
+        return self.message
