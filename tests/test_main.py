b54662d086c8eb98a8bfb01cef5aaf40218660e8
vaclav-2012 <38167104+vaclav-2012@users.noreply.github.com>
2020-05-29 19:38:07 +0200

    Implement `--debug` CLI option

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,3 +23,24 @@ def test_check_version():
     cli_result = runner.invoke(main, ["--version"])
     assert cli_result.exit_code == 0
     assert "slow-start-rewatch, version" in cli_result.output
+
+
+def test_debug(request):
+    """
+    Test the launch with the ``--debug`` option.
+
+    The logging level should be changed to ``DEBUG``.
+
+    To avoid affecting other tests the logging level is changed back to the
+    original value during the teardown.
+    """
+    logger = logging.getLogger()
+    request.addfinalizer(lambda: logger.setLevel(logging.CRITICAL))
+
+    assert logger.getEffectiveLevel() == logging.CRITICAL
+
+    runner = CliRunner()
+    cli_result = runner.invoke(main, ["--debug"])
+
+    assert logger.getEffectiveLevel() == logging.DEBUG
+    assert cli_result.exit_code == 0
