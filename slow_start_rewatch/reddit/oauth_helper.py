864a695ea67bb65b85ec66f6132d56c8611a74d0
vaclav-2012 <38167104+vaclav-2012@users.noreply.github.com>
2020-05-31 03:29:19 +0200

    Add the authorization with the refresh token

--- slow_start_rewatch/reddit/oauth_helper.py
+++ slow_start_rewatch/reddit/oauth_helper.py
@@ -5,11 +5,20 @@ import webbrowser
 
 import click
 from praw import Reddit
-from prawcore.exceptions import PrawcoreException
+from prawcore.exceptions import (
+    OAuthException,
+    PrawcoreException,
+    ResponseException,
+)
 from structlog import get_logger
 
 from slow_start_rewatch.config import Config
-from slow_start_rewatch.exceptions import AuthorizationError, RedditError
+from slow_start_rewatch.exceptions import (
+    AuthorizationError,
+    InvalidRefreshToken,
+    MissingRefreshToken,
+    RedditError,
+)
 from slow_start_rewatch.http_server import http_server
 
 BAD_REQUEST_ERROR = 400
@@ -26,6 +35,64 @@ class OAuthHelper(object):
         self.reddit = reddit
 
     def authorize(self) -> None:
+        """
+        Authorize the user.
+
+        1. Try to authorize using the stored refresh token.
+
+        2. If the refresh token is missing or invalid, authorize via OAuth.
+        """
+        try:
+            self.authorize_via_token()
+        except (MissingRefreshToken, InvalidRefreshToken) as exception:
+            click.echo(str(exception))
+            self.authorize_via_oauth()
+
+    def authorize_via_token(self) -> None:
+        """Authorize via the refresh token if the token exists."""
+        if not self.config.refresh_token:
+            log.warning("refresh_token_missing")
+            raise MissingRefreshToken
+
+        self.validate_refresh_token()
+
+    def validate_refresh_token(self) -> None:
+        """
+        Validate the refresh token.
+
+        The validation is performed by sending a request to the Reddit API.
+
+        The refresh token must be set when :class:`praw.Reddit` is initialized.
+        """
+        log.info("refresh_token_validate")
+        try:
+            scopes = self.reddit.auth.scopes()
+        except ResponseException as exception:
+            if exception.response.status_code == BAD_REQUEST_ERROR:
+                # Reddit throws a generic 400 error when the refresh token is
+                # not accepted.
+                self.config.refresh_token = None
+                log.error("refresh_token_invalid")
+                raise InvalidRefreshToken(
+                    "Cannot log in with the stored refresh token. " +
+                    "The token has been removed.",
+                )
+
+            log.exception("refresh_token_validation_error")
+            raise RedditError(
+                "Reddit error when validating refresh token.",
+            ) from exception
+        except OAuthException as exception:
+            log.exception("refresh_token_validation_error")
+            raise RedditError(
+                "Reddit OAuth error when validating refresh token.",
+            ) from exception
+
+        if not scopes:
+            log.error("refresh_token_validation_failed")
+            raise InvalidRefreshToken("Failed to validate the refresh token.")
+
+    def authorize_via_oauth(self) -> None:
         """
         Authorize via OAuth.
 
@@ -46,6 +113,8 @@ class OAuthHelper(object):
             "connect with your Reddit account so that it can submit and " +
             "edit posts on your behalf.\n" +
             "- The app cannot access your password.\n" +
+            "- To maintain access the app will store a 'refresh token' " +
+            "to your home directory.\n" +
             "- Press Ctrl+C if you would like to quit before completing " +
             "the authorization.",
         )
@@ -72,3 +141,5 @@ class OAuthHelper(object):
             raise AuthorizationError(
                 "Reddit hasn't provided the Refresh Token.",
             )
+
+        self.config.refresh_token = refresh_token
