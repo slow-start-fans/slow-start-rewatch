b8bdcb2ed4ba0dd7a875b48d2b02e419b0a1fc47
vaclav-2012 <38167104+vaclav-2012@users.noreply.github.com>
2020-06-14 13:09:30 +0200

    Refactor `RedditCutifier` tests

--- tests/test_reddit_cutifier.py
+++ tests/test_reddit_cutifier.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from typing import Optional
 from unittest.mock import call, patch
 
 import pytest
 from prawcore.exceptions import PrawcoreException
 
 from slow_start_rewatch.exceptions import RedditError
-from slow_start_rewatch.reddit.oauth_helper import OAuthHelper
+from slow_start_rewatch.post import Post
 from slow_start_rewatch.reddit.reddit_cutifier import RedditCutifier
 from tests.conftest import (
     HTTP_SERVER_HOSTNAME,
@@ -15,16 +16,20 @@ from tests.conftest import (
     MockConfig,
 )
 
+REDDIT_USER_AGENT = "Slow Start Rewatch Client"
+REDDIT_CLIENT_ID = "fluffy_client_id"
+REDDIT_CLIENT_SECRET: Optional[str] = None
 
-@patch.object(OAuthHelper, "authorize")
-@patch("praw.Reddit.__init__", return_value=None)
+
+@patch("slow_start_rewatch.reddit.reddit_cutifier.OAuthHelper")
+@patch("slow_start_rewatch.reddit.reddit_cutifier.Reddit")
 def test_authorize(
-    mock_reddit_init,
-    mock_oauth_helper_authorize,
+    mock_reddit,
+    mock_oauth_helper,
     reddit_cutifier_config,
 ):
     """
-    Test initialization and authorization.
+    Test the Reddit authorization.
 
     1. Ensure that the `Reddit` instance is initialized with the correct
        arguments.
@@ -32,75 +37,73 @@ def test_authorize(
     2. Check that the :meth:`OAuthHelper.authorize()` is called during the
        authorization.
     """
-    reddit_cutifier_config.refresh_token = REFRESH_TOKEN
     reddit_cutifier = RedditCutifier(reddit_cutifier_config)
 
-    reddit_cutifier.authorize()
-
-    reddit_config = reddit_cutifier_config["reddit"]
     redirect_uri = "http://{0}:{1}/".format(
-        reddit_cutifier_config["http_server.hostname"],
-        reddit_cutifier_config["http_server.port"],
+        HTTP_SERVER_HOSTNAME,
+        HTTP_SERVER_PORT,
     )
 
-    expected_call = call(
-        user_agent=reddit_config["user_agent"],
-        client_id=reddit_config["client_id"],
-        client_secret=reddit_config["client_secret"],
+    assert mock_reddit.call_args == call(
+        user_agent=REDDIT_USER_AGENT,
+        client_id=REDDIT_CLIENT_ID,
+        client_secret=REDDIT_CLIENT_SECRET,
         redirect_uri=redirect_uri,
         refresh_token=REFRESH_TOKEN,
     )
 
-    assert mock_reddit_init.call_args == expected_call
-    assert mock_oauth_helper_authorize.call_count == 1
+    reddit_cutifier.authorize()
+
+    assert mock_oauth_helper.return_value.authorize.called
 
 
-@patch.object(OAuthHelper, "authorize")
-@patch("praw.Reddit")
+@patch("slow_start_rewatch.reddit.reddit_cutifier.Reddit")
 def test_username(
     mock_reddit,
-    mock_oauth_helper_authorize,
     reddit_cutifier_config,
-    reddit,
 ):
     """Test that the username is retrieved from `PRAW` correctly."""
     reddit_cutifier = RedditCutifier(reddit_cutifier_config)
-    reddit_cutifier.reddit = reddit
+    reddit_user_me = mock_reddit.return_value.user.me
+
+    reddit_user_me.return_value.name = "cute_tester"
 
     assert reddit_cutifier.username == "cute_tester"
 
-    reddit.user.me.return_value = None
+    reddit_user_me.return_value = None
 
     with pytest.raises(AttributeError):
         assert reddit_cutifier.username
 
 
-@patch.object(OAuthHelper, "authorize")
-@patch("praw.Reddit")
-def test_submit_post(
+@patch("slow_start_rewatch.reddit.reddit_cutifier.Reddit")
+def test_submit_post_without_thumbnail(
     mock_reddit,
-    mock_oauth_helper_authorize,
     reddit_cutifier_config,
-    reddit,
-    post,
+    post: Post,
 ):
     """
-    Test that :meth:`RedditCutifier.submit_post()` uses `PRAW` correctly.
+    Test submitting a post.
 
-    1. Test a successful post submission.
+    1. Test successful submission:
 
-    2. Test an error handling during the submission.
+    Check that :meth:`Reddit.subreddit().submit()` is called with the correct
+    arguments.
+
+    2. Test handling of an exception raised by `PRAW`.
     """
     reddit_cutifier = RedditCutifier(reddit_cutifier_config)
-    reddit_cutifier.reddit = reddit
 
-    permalink = reddit_cutifier.submit_post(post)
+    subreddit = mock_reddit.return_value.subreddit.return_value
+    subreddit.submit.return_value.permalink = "slow_start_post_link"
 
-    assert permalink == "slow_start_post_link"
-    assert reddit.subreddit.return_value.submit.call_count == 1
+    assert reddit_cutifier.submit_post(post) == "slow_start_post_link"
+    assert subreddit.submit.call_args == call(
+        title=post.title,
+        selftext=post.body,
+    )
 
-    reddit.subreddit.return_value.submit.side_effect = PrawcoreException
-
+    subreddit.submit.side_effect = PrawcoreException
     with pytest.raises(RedditError):
         reddit_cutifier.submit_post(post)
 
@@ -108,14 +111,18 @@ def test_submit_post(
 @pytest.fixture()
 def reddit_cutifier_config():
     """Return the mock `Config` for testing the `RedditCutifier`."""
-    return MockConfig({
+    config = MockConfig({
         "reddit": {
-            "client_id": "fluffy_client_id",
-            "client_secret": None,
-            "user_agent": "Slow Start Rewatch Client",
+            "user_agent": REDDIT_USER_AGENT,
+            "client_id": REDDIT_CLIENT_ID,
+            "client_secret": REDDIT_CLIENT_SECRET,
         },
         "http_server": {
             "hostname": HTTP_SERVER_HOSTNAME,
             "port": HTTP_SERVER_PORT,
         },
     })
+
+    config.refresh_token = REFRESH_TOKEN
+
+    return config
